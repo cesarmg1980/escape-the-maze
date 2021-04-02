eafd8e597e0160056f39492b14994848081de976
cesarmg1980 <cesar.m.giulietti@outlook.com>
2021-04-01 23:23:24 -0300

    added more logic to player's script

---
+++ characters/player/Player.gd
@@ -0,0 +1,29 @@
+extends "res://characters/Character.gd"
+
+signal moved
+signal dead
+signal grabbed_key
+signal win
+
+func _process(delta):
+	if can_move:
+		for dir in moves.keys():
+			if Input.is_action_pressed(dir):
+				if move(dir):
+					emit_signal('moved')
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Player_area_entered(area):
+	if area.is_in_group('enemies'):
+		emit_signal('dead')
+	if area.has_method('pickup'):
+		area.pickup()
+		if area.type == 'key_red':
+			emit_signal('grabbed_key')
+		if area.type == 'star':
+			emit_signal('win')
